6e2aa6cdbc523854f4e9f3530f709eed4d6a4b98
a.korchagin <a.korchagin@growapp.biz>
2017-02-15 12:56:38 +0300

    made lab1

---
+++ app/src/main/java/com/alexkorrnd/pmulab1/BaseActivity.kt
@@ -0,0 +1,7 @@
+package com.alexkorrnd.pmulab1
+
+import android.support.v7.app.AppCompatActivity
+
+open class BaseActivity: AppCompatActivity() {
+
+}
---
+++ app/src/main/java/com/alexkorrnd/pmulab1/MessageAdapter.kt
@@ -0,0 +1,5 @@
+package com.alexkorrnd.pmulab1
+
+/**
+ * Created by alex on 01.02.17.
+ */
